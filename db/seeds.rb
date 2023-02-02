201355c7599f45a9adbf452c70fc56151052ce02
Babajide Muritala <jide142000@yahoo.com>
2023-02-02 20:59:08 +0100

    fake articles created from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database"
+Article.destroy_all
+
+puts "Creating fake aricles"
+
+10.times do
+  article = Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(number: 1)
+  )
+  puts "Article with id: #{article.id} has been created"
+end
+
+puts "Finished"
